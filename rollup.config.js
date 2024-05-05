c2ff156e41ed7245dae77a54ec802c0d9683e91d
Jarrad Raumati <jarrad.raumati@gmail.com>
2024-05-05 17:16:00 +1200

    chore: update submodules

--- rollup.config.js
+++ rollup.config.js
@@ -3,7 +3,7 @@ import babel from '@rollup/plugin-babel';
 import html from '@web/rollup-plugin-html';
 import copy from 'rollup-plugin-copy';
 import { importMetaAssets } from '@web/rollup-plugin-import-meta-assets';
-import { terser } from 'rollup-plugin-terser';
+import { terser } from '@rollup/plugin-terser';
 import { generateSW } from 'rollup-plugin-workbox';
 import path from 'path';
 
